63d66eade7a5dec3d75c610b6a7747b04586a475
Fiya Yufita <fiyayufita@gmail.com>
2021-06-15 14:08:54 +0700

    update file kalori

--- resources/views/kalori.blade.php
+++ resources/views/kalori.blade.php
@@ -1,10 +1,106 @@
 @extends('template2')
-@section('title','Kalori')
+@section('title','Daftar Kalori')
 
 @section('isi')
 
 <div style="color: white;">
-    <h1>Coba coba</h1>
-</div>
+<div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/nasi.png" width="100" height="100">
+                            <p>Nasi (1 mangkok)&ensp; &ensp; &ensp;204 kalori </p>
+                        </div>
+                    </a>
+                </li>
 
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/daging.png" width="100" height="100">
+                            <p>Daging (1 porsi) &emsp; 245 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/mie.png" width="100" height="100">
+                            <p>Mie Instant (1 mangkok) &emsp; &emsp;340 kalori </p>
+                        </div>
+                    </a>
+                </li>  
+                <br/>  
+
+            </ul>
+        </div>
+
+        <div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/cokelat.png" width="100" height="100">
+                            <p>Cokelat (1 batang) &emsp; &emsp;313 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/keju.png" width="100" height="100">
+                            <p>Keju (1 mangkok) &emsp; &emsp;455 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/kentang.png" width="100" height="100">
+                            <p>Kentang Goreng (1 mangkok) &emsp; &emsp;222 kalori </p>
+                        </div>
+                    </a>
+                </li> 
+                <br/>   
+
+            </ul>
+        </div>
+
+        <div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/kacang.png" width="100" height="100">
+                            <p>Kacang (1 mangkok) &emsp; &emsp;828 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/ayam.png" width="100" height="100">
+                            <p>Ayam Goreng (1 porsi) &emsp; &emsp;&nbsp; &emsp;&emsp;781 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/makanan/telur.png" width="100" height="100">
+                            <p>Telur Goreng (1 butir) &emsp; &emsp;&emsp;&ensp;&nbsp;&emsp;89 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+            </ul>
+        </div>
+
+</div>
 @endsection
