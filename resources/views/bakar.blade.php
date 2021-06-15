5439a6e8968648bdd26c57a15872499d1b518325
Fiya Yufita <fiyayufita@gmail.com>
2021-06-15 14:08:40 +0700

    menambahkan file bakar

---
+++ resources/views/bakar.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+	<head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/template.css">
+        <title>Ayo Bakar Kalori</title>
+    </head>
+
+	<body id="bg">
+        <nav id="basic" style ="padding : 15px;">
+                <img src="/img/logo2.png" width="150" height="70" style= "margin-left :1rem;">
+                <a href="/" class="font" style= "margin-left :50rem;">Home</a>
+                <a href="/about" class="font">About</a>
+                <a href="#" class="font">Content <i class="fas fa-caret-down"></i></a>
+        </nav>
+        
+    <center>
+        <div class="font">
+            <h2><img src="/img/workout.png" width="75" height="75" style="margin-left : 10px;"/>Ayo Bakar Kalorimu!</h2>
+            <p>Berikut ini berbagai olahraga yang bisa kamu lakukan untuk membakar kalori </p>
+            <hr size="8px">
+        </div>
+
+        <div class="mt-5">
+            <ul>
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/berenang.png" width="100" height="100">
+                            <p>Berenang/jam 840 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/lari.png" width="100" height="100">
+                            <p>Lari/jam 1500 kalori </p>
+                        </div>
+                    </a>
+                </li>   
+                
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/tali.png" width="100" height="100"> 
+                            <p>Lompat Tali/jam 850 kalori </p>
+                        </div>
+                    </a>
+                </li> 
+                
+                <br/> 
+
+            </ul>
+        </div>
+
+        <div class="mt-5">
+            <ul>
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/tangga.png" width="100" height="100">
+                            <p>Naik Turun Tangga/jam &emsp;830 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/sepeda.png" width="100" height="100">
+                            <p>Bersepeda/jam 1000 kalori </p>
+                        </div>
+                    </a>
+                </li>   
+                
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="ss">
+                            <img src="/img/olahraga/boxing.png" width="100" height="100"> 
+                            <p>Kick Boxing/jam 800 kalori </p>
+                        </div>
+                    </a>
+                </li> 
+
+            </ul>
+        </div>
+
+    </center>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+		
+    </body>
+</html>
