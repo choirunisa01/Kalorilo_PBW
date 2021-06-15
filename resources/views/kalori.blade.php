9ed8a5f201a9f59b5ac819b0c1c6e6cca8b0304a
Fiya Yufita <fiyayufita@gmail.com>
2021-06-15 17:38:59 +0700

    edit halaman bakar kalori

--- resources/views/kalori.blade.php
+++ resources/views/kalori.blade.php
@@ -1,34 +1,38 @@
 @extends('template2')
-@section('title','Daftar Kalori')
+@section('title','Kalorilo | Daftar Kalori Makanan')
 
 @section('isi')
 
+<center>
 <div style="color: white;">
 <div class="mt-5">
             <ul style ="margin : 2rem;">
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/nasi.png" width="100" height="100">
-                            <p>Nasi (1 mangkok)&ensp; &ensp; &ensp;204 kalori </p>
+                            <p>Nasi (1 mangkok)</p>
+                            <p>&nbsp;204 kalori </p>
                         </div>
                     </a>
                 </li>
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/daging.png" width="100" height="100">
-                            <p>Daging (1 porsi) &emsp; 245 kalori </p>
+                            <p>Daging (1 porsi)</p>
+                            <p>&nbsp; 245 kalori </p>
                         </div>
                     </a>
                 </li>    
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/mie.png" width="100" height="100">
-                            <p>Mie Instant (1 mangkok) &emsp; &emsp;340 kalori </p>
+                            <p>Mie Instant (1 mangkok)</p>
+                            <p>&nbsp;340 kalori </p>
                         </div>
                     </a>
                 </li>  
@@ -41,27 +45,30 @@
             <ul style ="margin : 2rem;">
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/cokelat.png" width="100" height="100">
-                            <p>Cokelat (1 batang) &emsp; &emsp;313 kalori </p>
+                            <p>Cokelat (1 batang)</p>
+                            <p>&nbsp;313 kalori </p>
                         </div>
                     </a>
                 </li>
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/keju.png" width="100" height="100">
-                            <p>Keju (1 mangkok) &emsp; &emsp;455 kalori </p>
+                            <p>Keju (1 mangkok)</p>
+                            <p>&nbsp;455 kalori </p>
                         </div>
                     </a>
                 </li>    
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/kentang.png" width="100" height="100">
-                            <p>Kentang Goreng (1 mangkok) &emsp; &emsp;222 kalori </p>
+                            <p>Kentang Goreng (1 mangkok)</p>
+                            <p>&nbsp;222 kalori </p>
                         </div>
                     </a>
                 </li> 
@@ -74,33 +81,37 @@
             <ul style ="margin : 2rem;">
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/kacang.png" width="100" height="100">
-                            <p>Kacang (1 mangkok) &emsp; &emsp;828 kalori </p>
+                            <p>Kacang (1 mangkok)</p>
+                            <p>&nbsp;828 kalori </p>
                         </div>
                     </a>
                 </li>
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/ayam.png" width="100" height="100">
-                            <p>Ayam Goreng (1 porsi) &emsp; &emsp; &emsp;&emsp;781 kalori </p>
+                            <p>Ayam Goreng (1 porsi)</p>
+                            <p>&nbsp;781 kalori </p>
                         </div>
                     </a>
                 </li>    
 
                 <li>
                     <a href="/kalori" class="fontmenu">
-                        <div id="card">
+                        <div>
                             <img src="/img/makanan/telur.png" width="100" height="100">
-                            <p>Telur Goreng (1 butir) &emsp; &emsp;&emsp;&ensp;&nbsp;&emsp;89 kalori </p>
+                            <p>Telur Goreng (1 butir)</p>
+                            <p>&nbsp;89 kalori </p>
                         </div>
                     </a>
                 </li>    
 
             </ul>
         </div>
-
 </div>
+</center>
+
 @endsection
