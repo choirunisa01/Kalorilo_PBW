276ff5761aad3ce2d58f2090246236765ccbe136
Fiya Yufita <fiyayufita@gmail.com>
2021-06-15 14:09:11 +0700

    menambahkan file kalori minuman

---
+++ resources/views/kaloriminuman.blade.php
@@ -0,0 +1,106 @@
+@extends('template2')
+@section('title','Daftar Kalori')
+
+@section('isi')
+
+<div style="color: white;">
+<div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                            <img src="/img/minuman/susu.png" width="100" height="100">
+                            <p>Susu (1 gelas)&ensp; &ensp; &ensp;122 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/kopi.png" width="100" height="100">
+                            <p>Kopi (1 gelas)&ensp; &ensp; &ensp;&ensp;2 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/mangga.png" width="100" height="100">
+                            <p>Jus Mangga (1 gelas)&ensp; &ensp;&emsp; &emsp; &emsp; &ensp; &ensp;107 kalori </p>
+                        </div>
+                    </a>
+                </li>  
+                <br/>  
+
+            </ul>
+        </div>
+
+        <div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/jeruk.png" width="100" height="100">
+                            <p>Jus Jeruk (1 gelas)&ensp; &ensp; &emsp; &emsp; &emsp;112 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/sirup.png" width="100" height="100">
+                            <p>Sirup (1 gelas)&ensp; &ensp; &ensp;850 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/soda.png" width="100" height="100">
+                            <p>Soda (1 gelas)&ensp; &ensp;&nbsp; &ensp;140 kalori </p>
+                        </div>
+                    </a>
+                </li> 
+                <br/>    
+
+            </ul>
+        </div>
+
+        <div class="mt-5">
+            <ul style ="margin : 2rem;">
+                <li>
+                    <a href="/kalori" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/anggur.png" width="100" height="100">
+                            <p>Jus Anggur (1 gelas)&ensp; &ensp;&emsp; &emsp; &emsp; &ensp; &ensp;154 kalori </p>
+                        </div>
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/alpukat.png" width="100" height="100">
+                            <p>Jus Alpukat (1 gelas)&ensp; &ensp;&emsp; &emsp; &emsp; &ensp; &ensp;195 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+                <li>
+                    <a href="#" class="fontmenu">
+                        <div id="card">
+                        <img src="/img/minuman/apel.png" width="100" height="100">
+                            <p>Jus Apel (1 gelas)&ensp; &ensp;&emsp; &emsp; &emsp; &ensp; &ensp;117 kalori </p>
+                        </div>
+                    </a>
+                </li>    
+
+            </ul>
+        </div>
+
+</div>
+@endsection
